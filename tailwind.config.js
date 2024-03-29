3f7a69f374527d557c526b025d3a3addcb94c0b8
evan-chernicky <echernicky@gmail.com>
2024-03-29 13:12:00 -0400

    Initial commit'

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const theme = require('./theme.json');
+const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './*.php',
+        './**/*.php',
+        './resources/css/*.css',
+        './resources/js/*.js',
+        './safelist.txt'
+    ],
+    theme: {
+        container: {
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '0rem'
+            },
+        },
+        extend: {
+            colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme)),
+            fontSize: tailpress.fontSizeMapper(tailpress.theme('settings.typography.fontSizes', theme))
+        },
+        screens: {
+            '2xl': {'max': '1535px'},
+            'xl': {'max': '1279px'},
+            'lg': {'max': '1023px'},
+            'md': {'max': '767px'},
+            'sm': {'max': '639px'},
+          },
+          container: {
+            center: true,
+            padding: '2rem',
+          },
+    },
+    plugins: [
+        tailpress.tailwind
+    ]
+};
